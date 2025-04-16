e680d72ec3ec81f57903cdc4562314bb884f86b8
Murilo <127051508+murilo587@users.noreply.github.com>
2025-04-16 02:48:26 -0300

    feat: implementar cor de fundo dinâmica no componente ApiCard

--- src/app/components/ApiCard/ApiCard.jsx
+++ src/app/components/ApiCard/ApiCard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const ApiCard = (props) => {
     return (
-      <tr className="text-gray-600 h-15">
-        <td className="px-4 py-0.5">{props.nome}</td>
-        <td className="px-4 py-0.5">{props.desc}</td>
-        <td className="px-4 py-0.5">{props.caminho}</td>
+      <tr className= {`${props.bgColor} h-15 text-gray-600`}>
+        <td className="px-10 py-0.5">{props.nome}</td>
+        <td className="px-10 py-0.5">{props.desc}</td>
+        <td className="px-10 py-0.5">{props.caminho}</td>
         <td className="py-0.5">
           <button className="bg-white text-[#B20000] border-2 border-[#B20000] px-8 py-1 rounded-xl font-semibold">
             Visualizar Endpoints
