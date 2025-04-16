a9c431b8c91f96ea30f1cea77c6e11771a589f89
Murilo <127051508+murilo587@users.noreply.github.com>
2025-04-15 21:39:12 -0300

    wip: primeira implementação do componente Footer

---
+++ api-integration/src/app/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+import Logo_sp from "../../../../public/images/logo_sp.svg"
+
+export default function Footer() {
+  return (
+    <footer className="bg-black border-gray-200 dark:bg-black w-full">
+      <div className="container mx-auto">
+        <div className="footer-logo relative h-[80px] w-[140px] mx-auto">
+          <Image
+            src={Logo_sp}
+            alt="são paulo"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
+    </footer>
+  );
+}
