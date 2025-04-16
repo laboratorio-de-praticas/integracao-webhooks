a019b0884b740badd34db8f54bc893588c08a23c
Murilo <127051508+murilo587@users.noreply.github.com>
2025-04-15 22:19:19 -0300

    feat: adicionar domínios públicos para as imagens

--- api-integration/next.config.mjs
+++ api-integration/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['saopaulo.sp.gov.br','www.cps.sp.gov.br'],
+    },
+  };
+  
+  export default nextConfig;
+  
