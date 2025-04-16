16a97a208f39308fd726bd8c9ad27280f9a9b338
Murilo <127051508+murilo587@users.noreply.github.com>
2025-04-16 01:42:11 -0300

    feat: componente Card de API

---
+++ src/app/components/ApiCard/ApiCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const ApiCard = (props) => {
+    return (
+      <tr className="text-gray-600 h-15">
+        <td className="px-4 py-0.5">{props.nome}</td>
+        <td className="px-4 py-0.5">{props.desc}</td>
+        <td className="px-4 py-0.5">{props.caminho}</td>
+        <td className="py-0.5">
+          <button className="bg-white text-[#B20000] border-2 border-[#B20000] px-8 py-1 rounded-xl font-semibold">
+            Visualizar Endpoints
+          </button>
+        </td>
+      </tr>
+    );
+  };
+  
+  
