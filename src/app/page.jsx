bebf6a900b319b6c3d0e6d1f15f3d3cc20730cb6
Murilo <127051508+murilo587@users.noreply.github.com>
2025-04-16 00:53:35 -0300

    feat: estrutura da página inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,13 +4,26 @@ import Footer from "../app/components/Footer/Footer";
 import NavBar from "../app/components/NavBar/NavBar";
 
 
-export default function Home(){
+export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <NavBar/>
-      <main className="flex-1">
-        <p>Hello world!</p>
+      <NavBar />
+      <main className="flex-1 flex justify-center bg-white">
+        <section className="flex-1 flex items-center flex-col">
+          <div className="w-4/5 text-black h-1/5 mt-20">
+            <h1 className="text-[#1A6C7C] text-4xl font-medium">Seleção de APIs</h1>
+            <hr></hr>
+            <p className="mt-10">Selecione a API:</p>
+          </div>
+          <div className="w-4/5 text-black h-25/50 border-gray-400 border-2 rounded-md">
+            <div className="flex gap-[15%] px-8 py-3">
+              <p>Nome</p>
+              <p>Descrição</p>
+              <p>Caminho</p>
+            </div>
+          </div>
+        </section>
       </main>
       <Footer />
     </div>
